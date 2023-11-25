dfa3689f06634c30f262c053754527ae9eaacd50
Ankush Prajapati <ankushpra49054@gmail.com>
2023-11-25 19:36:10 +0530

    add new APIkey

--- lib/constant/api_constant.dart
+++ lib/constant/api_constant.dart
@@ -1,2 +1,2 @@
 String BASE_URL="https://api.openai.com/v1";
-String API_KEY="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";
+String API_KEY="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";
